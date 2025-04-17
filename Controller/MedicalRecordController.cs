cfa86d6dc92bb18bac8a265fb19bbc1de7af1351
Ak-0609 <akansha.kumar93@gmail.com>
2025-04-17 17:56:38 -0400

    added authorization to medical records

--- Controller/MedicalRecordController.cs
+++ Controller/MedicalRecordController.cs
@@ -11,7 +11,7 @@ namespace MyBackendApi.Controller
 {
     [ApiController]
     [Route("api/[controller]")]
-    // [Authorize]
+    [Authorize]
     public class MedicalRecordController : ControllerBase
     {
         private readonly Data.AppDbContext dbContext;
